ed71a35017fb41c94f83f853f5f62b7deca71427
dayah24 <149245277+dayah24@users.noreply.github.com>
2023-11-29 12:54:14 +0700

    Update class Character.java

--- class Character.java
+++ class Character.java
@@ -4,6 +4,12 @@
  */
 package pkg22166034.pbo.lat59;
 
+* Nama         : Nur Hidayah
+* NIM          : 22166034 
+* Mata Kuliah  : PBO 
+* Prodi        : Sistem Informasi
+// Semester 3
+
 // Kelas Utama
 class Character {
     protected String name;
